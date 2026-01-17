eb35bce0f5ad0f5e4174ca5a8f66f6037b351e5a
rajuvemula <rj.vemula81@gmail.com>
2026-01-17 22:44:03 +0530

    add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,32 @@ import { Box } from '@chakra-ui/react'
 export const metadata = {
   title: 'Persistence Docs',
   description: 'Documentation for Persistence One ecosystem',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
+  openGraph: {
+    title: 'Persistence Docs',
+    description: 'Documentation for Persistence One ecosystem',
+    type: 'website',
+    url: 'https://docs.persistence.one',
+    siteName: 'Persistence Docs',
+    images: [
+      {
+        url: '/images/header_image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Persistence Docs',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Persistence Docs',
+    description: 'Documentation for Persistence One ecosystem',
+    images: ['/images/header_image.jpg'],
+  },
 }
 
 export default function RootLayout({
